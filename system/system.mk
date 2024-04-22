b9383301bdb5d9b6f54cee7dfac8cf80ddc09e17
Anna Babu Palathingal <148897727+annaibm@users.noreply.github.com>
2024-04-22 12:22:46 -0400

    Quote

--- system/system.mk
+++ system/system.mk
@@ -74,7 +74,7 @@ endif
 define SYSTEMTEST_CMD_TEMPLATE
 perl $(SYSTEMTEST_RESROOT)$(D)STF$(D)stf.core$(D)scripts$(D)stf.pl \
   -test-root=$(Q)$(SYSTEMTEST_RESROOT)$(D)STF;$(SYSTEMTEST_RESROOT)$(D)aqa-systemtest$(OPENJ9_PRAM)$(Q) \
-  -systemtest-prereqs=$(Q)$(LIB_DIR)(Q) \
+  -systemtest-prereqs=$(Q)$(LIB_DIR)$(Q) \
   -java-args=$(SQ)$(JAVA_ARGS)$(SQ) \
   -results-root=$(REPORTDIR)
 endef
