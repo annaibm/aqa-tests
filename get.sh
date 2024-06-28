fedc1aa2c4e38ebe515556e7d5b149a9e75dfcae
Anna Babu Palathingal <148897727+annaibm@users.noreply.github.com>
2024-06-28 14:04:05 -0400

    added lines into jenkins

--- get.sh
+++ get.sh
@@ -638,14 +638,6 @@ getVendorTestMaterial() {
 			fi
 		fi
 
-		if [[ "$repoURL" =~ "SVTTestRepo" ]]; then
-    		export IS_SVT_TESTREPO="true"
-    		echo "IS_SVT_TESTREPO is set to $IS_SVT_TESTREPO"
-		else
-    		export IS_SVT_TESTREPO="false"
-    		echo "IS_SVT_TESTREPO is set to $IS_SVT_TESTREPO"
-		fi
-
 		echo "git clone ${branchOption} $repoURL $dest"
 		git clone -q --depth 1 $branchOption $repoURL $dest
 
